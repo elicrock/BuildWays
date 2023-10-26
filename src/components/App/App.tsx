3a120fb536a79773ba2c930b11e0825965d02fea
Viktor Filippov <mozgfoxtrot@gmail.com>
2023-10-27 02:55:00 +0400

    create basket adaptive

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,16 +5,18 @@ import Catalogy from '../Catalogy/Catalogy';
 import Register from '../../pages/Register/Register';
 import Login from '../../pages/Login/Login';
 import Basket from '../Basket/Basket';
+import PageNotFound from '../../pages/PageNotFound/PageNotFound';
 
 function App() {
   return (
-    <> 
+    <>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/catalogy' element={<Catalogy />} />
-        <Route path='/signup' element={<Register />} />
-        <Route path='/signin' element={<Login />} />
-        <Route path='/basket' element={<Basket />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/catalogy" element={<Catalogy />} />
+        <Route path="/signup" element={<Register />} />
+        <Route path="/signin" element={<Login />} />
+        <Route path="/basket" element={<Basket />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </>
   );
--- src/components/Basket/Basket.tsx
+++ src/components/Basket/Basket.tsx
@@ -3,20 +3,35 @@ import BasketCardItem from './BasketCardItem/BasketCardItem';
 
 function Basket() {
   return (
-    <section className='basket'>
-      <h1 className='basket__title'>Корзина</h1>
-      <div className='basket__container'>
-        <ul className='basket__list'>
-          <li className='basket__list-item'>Название</li>
-          <li className='basket__list-item'>Стоимость</li>
-          <li className='basket__list-item'>Количество</li>
-          <li className='basket__list-item'>Итого</li>
+    <section className="basket">
+      <h1 className="basket__title">Корзина</h1>
+      <div className="basket__container">
+        <ul className="basket__list">
+          <li className="basket__list-item">Название</li>
+          <li className="basket__list-item">Стоимость</li>
+          <li className="basket__list-item">Количество</li>
+          <li className="basket__list-item">Итого</li>
         </ul>
-        <ul className='basket__card-list'>
+        <ul className="basket__card-list">
           <BasketCardItem />
           <BasketCardItem />
           <BasketCardItem />
         </ul>
+        <div className="basket__box-total-amount">
+          <div className="basket__sum-section">
+            <span className="basket__sum">Сумма</span>
+            <span className="basket__sum-price">4550 ₽</span>
+          </div>
+          <div className="basket__sum-section">
+            <span>Скидка</span>
+            <span>-250 ₽</span>
+          </div>
+          <div className="basket__sum-section">
+            <span>К оплате</span>
+            <span>4200 ₽</span>
+          </div>
+          <button className="basket__">Перейти к оформлению</button>
+        </div>
       </div>
     </section>
   );
