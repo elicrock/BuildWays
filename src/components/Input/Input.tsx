4fb9f354c1bb027844f8ff5daeea59a01beca0e8
Viktor Filippov <mozgfoxtrot@gmail.com>
2023-11-27 01:19:46 +0400

    fix styles input + errors

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,17 +1,18 @@
 import './Input.css';
 
 interface InputProps {
+  labelName?: string;
   type: string;
   placeholder: string;
   inputId: string;
   // errors?;
 }
 
-const Input: React.FC<InputProps> = ({ type, placeholder, inputId }: InputProps) => {
+const Input: React.FC<InputProps> = ({ labelName, type, placeholder, inputId }: InputProps) => {
   return (
     <>
       <label className="form__label" htmlFor={inputId}>
-        {placeholder}
+        {labelName}
         <input className="form__input" id={inputId} placeholder={placeholder} type={type} />
       </label>
       <span className="form__input-error">Ошибка</span>
